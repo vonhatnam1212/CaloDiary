7db8ca7437a42142a1851effebaf625af1c4ceb0
namvn <vonhatnam.work@gmail.com>
2025-03-22 15:28:06 +0000

    feat: add chat with openAI

---
+++ app/src/main/java/com/example/calodiary/ChatAdapter.java
@@ -0,0 +1,49 @@
+package com.example.calodiary;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+    private List<ChatMessage> chatMessages;
+
+    public ChatAdapter(List<ChatMessage> chatMessages) {
+        this.chatMessages = chatMessages;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        ChatMessage chatMessage = chatMessages.get(position);
+        holder.messageTextView.setText(chatMessage.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return chatMessages.get(position).isUser() ? R.layout.item_user_message : R.layout.item_ai_message;
+    }
+
+    public static class ChatViewHolder extends RecyclerView.ViewHolder {
+        TextView messageTextView;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageTextView = itemView.findViewById(R.id.message_text);
+        }
+    }
+}
